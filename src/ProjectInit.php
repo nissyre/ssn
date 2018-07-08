007e9938e5f8907aa0a385560dab8d00d2ce6cf5
nissyre <nissyre@gmail.com>
2018-07-08 22:03:31 +0200

    Working

--- src/ProjectInit.php
+++ src/ProjectInit.php
@@ -4,29 +4,33 @@ namespace Nissyre;
 
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Output\OutputInterface;
-
+use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder;
 
 class ProjectInit extends Command
 {
     protected function configure()
     {
         $this
-            // the name of the command (the part after "bin/console")
             ->setName('app:project-init')
-
-            // the short description shown while running "php bin/console list"
             ->setDescription('Creates a new empty project.')
-
-            // the full command description shown when running the command with
-            // the "--help" option
             ->setHelp('Use to init new project in empty directory')
-        ;
+            ->addArgument('root_dir', InputArgument::REQUIRED, 'Package directory');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
         $output->write('Project-init');
-    }
+        $root_dir = $input->getArgument('root_dir');
+        $fileSystem = new Filesystem();
+        if ($fileSystem->exists($root_dir)) {
+            //todo
+        }
+        else {
+            $output->write('Directory doesn\'t exist');
+        }
 
+    }
 }
